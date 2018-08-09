12aa0c40fe08e349b87d3ac5489baaa0d8270dc1
Sander Jetten <sanderjetten@gmail.com>
2018-08-09 22:29:31 +0200

    Added custom pipe for replacing char with space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import {FormsModule} from '@angular/forms';
 import { WelcomeComponent } from './home/welcome.component';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
