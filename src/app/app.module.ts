6bb94a632f5a659d3fada21f2e62c979bfed4a63
Sander Jetten <sanderjetten@gmail.com>
2018-08-09 22:19:42 +0200

    - added OnInit hook to product - added PROD-build and serve scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import {FormsModule} from '@angular/forms';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
